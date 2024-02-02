18b7f65036384301f472bdaefe11090cf17d9c82
Israel Akinoso <akinosoissy@gmail.com>
2024-02-02 13:07:18 +0000

    User JWT tokens on registration

--- backend/users/api/views.py
+++ backend/users/api/views.py
@@ -5,32 +5,56 @@ from rest_framework.response import Response
 from rest_framework import status
 from users.api.permissions import IsCustomer, IsOwner
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-
 from rest_framework_simplejwt.tokens import RefreshToken
 
-# def get_tokens_for_user(user):
-#     refresh = RefreshToken.for_user(user)
 
-#     return {
-#         'refresh': str(refresh),
-#         'access': str(refresh.access_token),
-#     }
 
 class CreateRestaurantView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = RestaurantSerializer(data=request.data)
+        data = {}
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            restaurant = serializer.save()
+            
+            data['response'] = "Registration Successful!"
+            data['username'] = restaurant.user.username
+            data['email'] = restaurant.user.email
+            data['slug'] = restaurant.user.slug
+            data['name'] = restaurant.name
+            data['is_owner'] = restaurant.user.is_owner            
+            
+            refresh = RefreshToken.for_user(restaurant.user)
+            data['token'] = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            }
+            
+            return Response(data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
 
     
 class CreateCustomerView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = CustomerSerializer(data=request.data)
+        data = {}
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            customer = serializer.save()
+            
+            data['response'] = "Registration Successful!"
+            data['username'] = customer.user.username
+            data['email'] = customer.user.email
+            data['slug'] = customer.user.slug
+            data['first_name'] = customer.user.first_name
+            data['last_name'] = customer.user.last_name
+            data['is_customer'] = customer.user.is_customer            
+            
+            refresh = RefreshToken.for_user(customer.user)
+            data['token'] = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            }
+            
+            return Response(data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
