0bbd056069c83443ea52446a08e6b2fb7f7d7b0a
Israel Akinoso <akinosoissy@gmail.com>
2024-02-27 23:22:16 +0000

    fix compiler options

--- frontend/components/header/Header.jsx
+++ frontend/components/header/Header.jsx
@@ -10,13 +10,13 @@ export default function Header() {
         className="absolute inset-0 h-full w-full object-cover object-center"
       />
 
-      <div className="relative flex items-center justify-center bg-gradient-to-t from-gray-900/70 to-gray-900/65 py-52">
+      <div className="relative flex items-center justify-center bg-gradient-to-t from-gray-900/70 to-gray-900/75 py-52">
         <div className="mx-auto max-w-5xl px-6 lg:px-8">
           <div className="mx-auto text-center lg:mx-0">
             <h2 className="font-clash text-4xl font-bold leading-none tracking-normal text-white sm:text-6xl sm:leading-normal">
               Order your favourite food online
             </h2>
-            <p className="mt-4 font-geist leading-8 text-gray-200 sm:mt-0 sm:text-lg">
+            <p className="mt-4 font-geist leading-8 text-gray-100 sm:mt-0 sm:text-lg">
               Fast, fresh, and convenient food delivery.
             </p>
 
